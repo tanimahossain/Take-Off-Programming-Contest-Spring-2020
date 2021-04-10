f06e3af49504e43af2bfd5cca3c5200b159f0987
tanimahossain <56477789+tanimahossain@users.noreply.github.com>
2021-04-10 13:42:57 +0600

    Create H.cpp

---
+++ H.cpp
@@ -0,0 +1,43 @@
+///time complexity : O(t*(n+q))
+///space complexity: O(n)
+#include <stdio.h>
+int main(){
+    int cases=0,test;
+    scanf("%d",&test);
+    while(cases++!=test){
+        char ch[100005];
+        int n,i,cnt[36];
+        scanf("%d%s",&n,ch);
+        for(i=0;i<36;i++)cnt[i]=0;
+        for(i=0;i<n;i++){
+            cnt[ch[i]-'a']++;
+        }
+        int q;
+        scanf("%d",&q);
+        printf("Case %d:\n",cases);
+        while(q--){
+            int j;
+            char c;
+            scanf("%d %c",&j,&c);
+            cnt[ch[j-1]-'a']--;
+            ch[j-1]=c;
+            cnt[c-'a']++;
+            int flag=0;
+            int ans=0;
+            for(i=0;i<=25;i++){
+                if(cnt[i]%2==1){
+                    flag=1;
+                    ans+=cnt[i]-1;
+                }
+                else ans+=cnt[i];
+            }
+            if(flag==1){
+                ans++;
+            }
+            printf("%d\n",ans);
+        }
+    }
+}
+
+
+
