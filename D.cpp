75257a79ec5cee6ef666342ed52db50f8983f239
tanimahossain <56477789+tanimahossain@users.noreply.github.com>
2021-04-10 13:44:42 +0600

    Create D.cpp

---
+++ D.cpp
@@ -0,0 +1,21 @@
+//Solution in C Language.
+
+#include <stdio.h>
+int main(){
+    freopen("data1.in", "r", stdin);
+    freopen("data1.out", "w", stdout);
+    int t, cs = 0;
+    scanf("%d", &t);
+    while(t--){
+        int n, k, ans = 0;
+        scanf("%d %d", &n, &k);
+        while(n--){
+            int reg;
+            scanf("%d", &reg);
+            ans += (reg % k);
+        }
+        printf("Case %d: %d\n", ++cs, ans);
+    }
+    
+    return 0;
+}
